b5a020961592b8269e32fc757ee2b708af1a3806
Mayank Gupta <mayankkumargupta03@gmail.com>
2024-04-14 02:09:44 +0530

    dssdf

--- src/components/CallUi.tsx
+++ src/components/CallUi.tsx
@@ -1,11 +1,15 @@
 "use client"
+import { BASE_URL } from '@/constants';
 import { CallControls, CallingState, SpeakerLayout, StreamTheme, useCall, useCallStateHooks } from '@stream-io/video-react-sdk'
+import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import React, { useEffect } from 'react'
+import { useCookies } from 'react-cookie';
+import { ToastContainer, toast } from 'react-toastify';
 
 
-const CallUi = () => {
-
+const CallUi = ({id}:any) => {
+const [cookies] = useCookies(['token']);
     const { useCallCallingState, useParticipantCount,    useLocalParticipant,
         useRemoteParticipants, } = useCallStateHooks();
         const router = useRouter()
@@ -14,12 +18,18 @@ const CallUi = () => {
   const remoteParticipants = useRemoteParticipants();
  useEffect(()=>{
     if(callingState===CallingState.LEFT){
-        router.push(`/meeting/end`)
+        axios.put(`${BASE_URL}/booking/end?booking_id=${id}`,{},{headers:{Authorization:`Bearer ${cookies.token}`}}).then((res)=>{
+            router.replace(`/meeting/end?id=${id}`)
+                })
+                .catch((error:any)=>{
+                    toast.error(error.message);
+                })
     }
  },[callingState])
   return (
   
   <>
+  <ToastContainer/>
   {
 (callingState!== CallingState.JOINED) ? 
 (
